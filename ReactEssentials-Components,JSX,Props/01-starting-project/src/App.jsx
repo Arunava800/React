12be21590dde271c51d3927327050b3b1ee2f632
Arunava <arunava800@gmail.com>
2024-06-15 22:19:59 +0530

    MakingComponentsReusableWithProps

--- ReactEssentials-Components,JSX,Props/01-starting-project/src/App.jsx
+++ ReactEssentials-Components,JSX,Props/01-starting-project/src/App.jsx
@@ -1,9 +1,13 @@
 import reactImg from './assets/react-core-concepts.png'
+import componentsImg from './assets/components.png'
 const reactDescriptions = ["Fundamental","Crucial", "Core"];
 
 function getRandomInt(max){
   return Math.floor(Math.random()* (max+1));
 }
+
+// When working with react functions, there is only one function which can be called any thing but it generally
+// named as props
 
 function Header(){
   const descriptions  = reactDescriptions[getRandomInt(2)];
@@ -17,13 +21,46 @@ function Header(){
     </p>
   </header>);
 
+
 }
+
+function CoreConcepts(props){
+  return(
+  <li>
+    <img src = {props.image} alt={props.title} />
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
+
+  </li>);
+}
+
 function App() {
   return (
     <div>
       <Header />
+  
       <main>
-        <h2>Time to get started!</h2>
+      {/* Core concepts id is used for styling used in index.css file */}
+        {/* Custom attributes can be added to the custom components */}
+            {/* "tilte" and "description" attributes are known as props as the concept of configuring components
+            is called props.  */}
+        <section id = "core-concepts"> 
+          <h2>Time to get started! </h2>
+          <ul>
+            <CoreConcepts title="Components" 
+             description="The Core-UI building block."
+            image = {componentsImg}
+            /> 
+            <CoreConcepts title = "props" />
+            <CoreConcepts />
+            <CoreConcepts />
+
+
+
+
+          </ul>
+        </section>
+        
       </main>
     </div>
   );
